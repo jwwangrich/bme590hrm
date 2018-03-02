7766dbc53a2392bee244af699c5dcc3a76a4de9e
jwwangrich <jwwangrich@gmail.com>
2018-03-02 11:46:33 -0500

    travis err04

--- HeartRateMonitor/test_data/hrm_module/test_duration.py
+++ HeartRateMonitor/test_data/hrm_module/test_duration.py
@@ -1,10 +1,14 @@
 def test_duration():
-    from hrm_module.myrhm import Myrhm
+    from myrhm import Myrhm
     import math
     import pytest
-    duration_output_1 = duration([1, 2, 3, 4, 5])
-    assert duration_output_1 == 4
-    with pytest.raueses(ValueError):
+    duration_output_1 = [1, 2, 3, 4, 5]
+
+    output_1 = Myrhm(duration_output_1)
+
+    assert output_1 == 4
+
+    with pytest.raises(ValueError):
         duration(-2)
     with pytest.raises(TypeError):
         duration('time')
